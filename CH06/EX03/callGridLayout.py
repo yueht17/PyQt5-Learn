ee4c20de0283f4c14d73dc690583d8e73dbc4b53
haitao <yueht17@gmail.com>
2020-07-13 14:28:11 +0800

    Using Grid Layout

---
+++ CH06/EX03/callGridLayout.py
@@ -0,0 +1,12 @@
+#! usr/bin/env python
+# -*- coding:utf-8 -*-
+import sys
+from demoGridLayout import *
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
